89a33a77b28c2b683e6e06afb3b15011b0a54845
Anna Henningsen <anna@addaleax.net>
2017-11-24 02:42:10 +0100

    Fixup

--- get-paths.js
+++ get-paths.js
@@ -1,4 +1,5 @@
 'use strict';
+const uvloop = require('get-uv-event-loop-napi-h');
 const path = require('path');
-exports.include = path.resolve(__dirname, 'include');
-exports.gyp = '';
+exports.include = path.resolve(__dirname, 'include') + ' ' + uvloop.include;
+exports.gyp = uvloop.gyp;
