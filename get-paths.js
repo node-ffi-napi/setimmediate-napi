002c7d62c85f6fa8fd5a41cbec0e500ca2b9a960
Anna Henningsen <anna@addaleax.net>
2020-01-24 22:48:08 +0100

    Fix paths-with-spaces handling for Windows builds
    Refs: https://github.com/node-ffi-napi/weak-napi/issues/21

--- get-paths.js
+++ get-paths.js
@@ -2,6 +2,6 @@
 const uvloop = require('get-uv-event-loop-napi-h');
 const symbol = require('get-symbol-from-current-process-h');
 const path = require('path');
-exports.includeRaw = path.resolve(__dirname, 'include') + ' ' + uvloop.includeRaw + ' ' + symbol.includeRaw;
-exports.include = exports.includeRaw.replace(/\\/g, '\\\\');
+exports.includeRaw = [path.resolve(__dirname, 'include')].concat(uvloop.includeRaw).concat(symbol.includeRaw);
+exports.include = exports.includeRaw.map(x => `"${x}"`.replace(/\\/g, '\\\\')).join(' ');
 exports.gyp = uvloop.gyp;
